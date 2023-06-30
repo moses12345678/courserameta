acf4d342ab655b26faffb7715e5b20d221aad5b5
diamoussa <diamoussa2014@gmail.com>
2023-06-30 02:13:49 -0400

    create a sementic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,13 @@
 import React from "react";
-import {Box, Flex} from "@chakra-ui/react";
+import Logo from '../images/Logo.svg';
 
 const Footer = () => {
   return (
-    <Box backgroundColor="#18181b">
+    <>
       <footer>
-        <Flex
-          margin="0 auto"
-          px={12}
-          color="white"
-          justifyContent="center"
-          alignItems="center"
-          maxWidth="1024px"
-          height={16}
-        >
-          <p>Pete • © 2022</p>
-        </Flex>
+        <img src={Logo} alt="logo footer"/>
       </footer>
-    </Box>
-  );
+    </>
+  )
 };
 export default Footer;
